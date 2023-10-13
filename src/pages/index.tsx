ec37da9bf79036cb4d8b27d80bfe382d5b3874bd
TonySilva7 <tony.deve7oper@gmail.com>
2023-10-12 22:23:35 -0300

    feat: implement carousel using keen-slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,44 @@
 import { HomeContainer, ProductContainer } from '@app/styles/module'
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
+
 import Image from 'next/image'
 import shirt1 from '@app/assets/camisetas/1.png'
 import shirt2 from '@app/assets/camisetas/2.png'
 import shirt3 from '@app/assets/camisetas/3.png'
 
 export default function Home() {
+  const [slideRef] = useKeenSlider<HTMLDivElement>({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
   return (
     <>
-      <HomeContainer>
-        <ProductContainer>
+      <HomeContainer ref={slideRef} className="keen-slider">
+        <ProductContainer className="keen-slider__slide">
           <Image src={shirt1} alt="Camiseta 1" width={520} height={480} />
           <footer>
             <strong>Camiseta X</strong>
             <span>R$ 79,90</span>
           </footer>
         </ProductContainer>
-        <ProductContainer>
+        <ProductContainer className="keen-slider__slide">
           <Image src={shirt2} alt="Camiseta 2" width={520} height={480} />
           <footer>
             <strong>Camiseta X</strong>
             <span>R$ 79,90</span>
           </footer>
         </ProductContainer>
-        <ProductContainer>
+        <ProductContainer className="keen-slider__slide">
+          <Image src={shirt3} alt="Camiseta 3" width={520} height={480} />
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </ProductContainer>
+        <ProductContainer className="keen-slider__slide">
           <Image src={shirt3} alt="Camiseta 3" width={520} height={480} />
           <footer>
             <strong>Camiseta X</strong>
